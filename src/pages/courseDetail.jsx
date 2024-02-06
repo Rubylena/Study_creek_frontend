f00e8f62edd74083d4a9f55341a70d5af7828e53
Damilola Adebowale <adebzdamzy@gmail.com>
2024-02-06 23:05:26 +0100

    upload & publish page

--- src/pages/courseDetail.jsx
+++ src/pages/courseDetail.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import '../styles/courseDetail.css'
+import React,{useState} from 'react'
+import '../styles/courseDetail.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function CourseDetail() {
+
+  const [image, setImage] = useState(null)
+  const navigate= useNavigate()
+
+  const uploadImage =(e)=>{
+    let reader = new FileReader()
+
+    if (e.target.files && e.target.files[0]) {
+      reader.onload = (e)=>{
+        setImage(e.target.result)
+      }
+      reader.readAsDataURL(e.target.files[0])
+
+    }
+  }
+
+  console.log(image)
   return (
     <div className='courseDetail'>
         <h2>Course Detail</h2>
@@ -22,12 +40,16 @@ export default function CourseDetail() {
              <h6 className='details-label'>Thumbnail:</h6>
              <div className='file-div'>
              <label htmlFor='file' className='file-label'> Choose Image</label>
-             <input type='file' id='file'/>
-             </div>
+             <input type='file' id='file' accept='image/*'
+             onChange={uploadImage}/>
+             {image && <img alt='' src={image} className='thumbnail-image'/>}
+             </div> 
             
             </div>
             
-            <button className='next-button'>Next</button>
+            <button className='next-button'
+              onClick={()=>navigate('/upload')}
+            >Next</button>
 
         </div>
     </div>
